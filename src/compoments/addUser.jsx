2ad7ed465a72b78363983d73cc722442d8970091
[faizii-x] <faizanramzan670@gmail.com>
2024-06-02 22:20:36 +0500

    taskingg

--- src/compoments/addUser.jsx
+++ src/compoments/addUser.jsx
@@ -19,30 +19,30 @@ function AddUser({ addUser }) {
 
   return (
     <form onSubmit={handleSubmit}>
-      <div className="md:flex grid justify-between gap-3">
+      <div className="md:flex grid md:justify-between justify-center gap-3 ">
         <input
           type="text"
           placeholder="Name"
           value={name}
           onChange={(e) => setName(e.target.value)}
-          className="w-full border border-orange-300 pl-2 rounded-lg hover:bg-gray-200"
+          className="w-full md:p-0 p-2 border border-orange-300 pl-2 rounded-lg hover:bg-gray-200 "
         />
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
-          className="w-full border border-orange-300 pl-2 rounded-lg hover:bg-gray-200"
+          className="w-full md:p-0 p-2 border border-orange-300 pl-2 rounded-lg hover:bg-gray-200"
         />
         <div
           type="submit"
-          className="bg-orange-400 w-[180px] hover:text-black cursor-pointer hover:bg-gray-200 border flex justify-center items-center p-2 rounded-lg text-white"
+          className=" bg-orange-400 lg:w-[180px] w-full hover:text-black cursor-pointer hover:bg-gray-200 border flex justify-center items-center p-2 rounded-lg text-white"
         >
           <button>Add User</button>
         </div>
       </div>
       {validationMessage && (
-          <div className="text-red-500 text-sm mt-2">
+          <div className="text-red-700 font-semibold text-sm mt-2">
             {validationMessage}
           </div>
         )}
